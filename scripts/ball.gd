4c8d66ee6388ba259193222fcec69edc14bc19b3
sarahmmcb <sarah.m.mcb@gmail.com>
2026-05-09 16:18:48 -0400

    make ball change speed based on where it hits the paddle

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,11 +2,15 @@ extends CharacterBody2D
 
 var speed := 300.0
 var is_active := false
+var paddle_width := 199.0  # width of texture scaled down by 0.5 since the Sprite is scaled by 0.5
+var speed_up_zone_width := 30.0
+var speed_up_zone_check := 0.0
 
 func _ready() -> void:
 	speed = speed + (20 * GameManager.level)
 	$CPUParticles2D.visible = false;
 	velocity = Vector2(speed, speed)
+	speed_up_zone_check = (paddle_width - (2.0 * speed_up_zone_width)) / 2.0
 	
 func _process(_delta: float) -> void:
 	if !is_active:
@@ -23,6 +27,18 @@ func _physics_process(delta: float) -> void:
 			velocity = velocity.bounce(collision.get_normal())
 			if collision.get_collider().has_method("hit"):
 				collision.get_collider().hit()
+			if collision.get_collider() == get_node("../paddleStatic"):
+				var paddle = get_node("../paddleStatic")
+				var collision_point = collision.get_position()
+				var paddle_position = paddle.position
+				if abs(collision_point.x - paddle_position.x) >= speed_up_zone_check:
+					print("speeding up")
+					var new_speed = clamp(velocity.length() + (0.5 * velocity.length()), 420.0, 1000.0)
+					velocity = velocity.normalized() * new_speed
+				else:
+					print("slowing down")
+					var new_speed = clamp(velocity.length() - (0.3 * velocity.length()), 420.0, 1000.0)
+					velocity = velocity.normalized() * new_speed
 		
 		# If we're not moving much in the y direction, spike it up in the air a bit
 		if (velocity.y > 0 and velocity.y < 100):
